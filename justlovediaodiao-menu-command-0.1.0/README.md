fd6ddec8000dc6e162b0230a58180d0f6a89ab80
yangxing <justlovediaodiao@outlook.com>
2020-08-29 15:02:08 +0800

    update filename args

--- justlovediaodiao-menu-command-0.1.0/README.md
+++ justlovediaodiao-menu-command-0.1.0/README.md
@@ -4,15 +4,15 @@ It adds a context menu to vscode and run command when be clicked.
 
 ### config
 
-Specify the commands by settings `commandMenu.commands`. The file name will be passed to the command as a argument when running command.  
+Specify the commands by settings `commandMenu.commands`. `command` is the command to run. `{}` in `command` will be replaced with related filename.  `name` is optional.
 
 ```conf
 commandMenu.commands = [
-    {"name": "echo", "command": "echo"},
-    {"name": "tail", "command": "tail -10"}
+    {"command": "echo test"},
+    {"name": "show filename", "command": "echo {}"}
 ]
 ```
 
-When you click `Run Command` context menu on file `hello.py` and pick `tail`, the command `tail -10 hello.py` will be executed.  
+When you click `Run Command` context menu on file `hello.py` and pick `show filename`, the command `echo "hello.py"` will be executed.  
 When there is no command configuration, it allows you to enter a command to execute.  
 When there is one command configuration, it directly executes the command.
--- justlovediaodiao-menu-command-0.1.0/extension.js
+++ justlovediaodiao-menu-command-0.1.0/extension.js
@@ -1,73 +1,76 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
-var terminal = undefined;
+var terminal = null;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    var last_input = undefined;
-    var last_pick = undefined;
+    var last_input = null;
+    var last_pick = null;
     // register command
-    var disposable = vscode.commands.registerCommand("commandMenu.runCommand", async commandContext => {
+    var disposable = vscode.commands.registerCommand('commandMenu.runCommand', async commandContext => {
         // get file path from event arg or current active text editor
-        var path = undefined;
+        var path = null;
         if (commandContext) {
             path = commandContext.path || commandContext._resourceUri.path;
-        }
-        else {
+        } else {
             var editor = vscode.window.activeTextEditor;
             if (editor && !editor.document.isUntitled) {
                 path = editor.document.fileName;
             }
         }
-        if (path) {
-            // get terminal
-            if (!terminal) {
-                terminal = vscode.window.createTerminal("command");
-            }
-            // get command to run
-            var config = vscode.workspace.getConfiguration("commandMenu");
-            var commands = config.commands;
-            var command = undefined;
-            if (commands.length == 0) {
-                var option = {placeHolder: "input a command to run", value: last_input ? last_input : ""};
-                command = await vscode.window.showInputBox(option);
-                if (command) {
-                    last_input = command
-                }
-            } else {
-                if (commands.length == 1) {
-                    command = commands[0].command;
-                } else {
-                    var items = commands.map(c => { return {label: c.name, description: c.command} });
-                    if (last_pick) {
-                        // put last pick to first
-                        var index = items.findIndex(i => i.label == last_pick);
-                        if (index > 0) {
-                            var first = items[index];
-                            for (i = index; i > 0; i--) {
-                                items[i] = items[i-1];
-                            }
-                            items[0] = first;
-                        }
-                    }
-                    var item = await vscode.window.showQuickPick(items, {placeHolder: 'select a command to run'});
-                    if (item) {
-                        last_pick = item.label;
-                        command = item.description;
-                    }
-                }
-            }
+        if (!path) {
+            return
+        }
+        // get terminal
+        var t = terminal || vscode.window.activeTerminal;
+        if (!t) {
+            terminal = vscode.window.createTerminal('command');
+            t = terminal;
+        }
+        // get command to run
+        var config = vscode.workspace.getConfiguration('commandMenu');
+        var commands = config.commands;
+        var command = null;
+        if (commands.length == 0) {
+            var option = {placeHolder: 'input a command to run', value: last_input ? last_input : ''};
+            command = await vscode.window.showInputBox(option);
             if (command) {
-                terminal.show();
-                terminal.sendText(`${command} "${path}"`);
+                last_input = command
             }
+        } else {
+            if (commands.length == 1) {
+                command = commands[0].command;
+            } else {
+                var items = commands.map(c => { return {label: c.name || '', description: c.command} });
+                if (last_pick) {
+                    // put last pick to first
+                    var index = items.findIndex(i => i.label == last_pick);
+                    if (index > 0) {
+                        var first = items[index];
+                        for (i = index; i > 0; i--) {
+                            items[i] = items[i-1];
+                        }
+                        items[0] = first;
+                    }
+                }
+                var item = await vscode.window.showQuickPick(items, {placeHolder: 'select a command to run'});
+                if (item) {
+                    last_pick = item.label;
+                    command = item.description;
+                }
+            }
+        }
+        if (command) {
+            command = command.replace('{}', `"${path}"`)
+            t.show();
+            t.sendText(command);
         }
     });
     // set to undefined when terminal closed
     vscode.window.onDidCloseTerminal(t => {
         if (t == terminal) {
-            terminal = undefined;
+            terminal = null;
         }
     })
 
@@ -79,7 +82,7 @@ exports.activate = activate;
 function deactivate() {
     if (terminal) {
         terminal.dispose();
-        terminal = undefined;
+        terminal = null;
     }
 }
 exports.deactivate = deactivate;
